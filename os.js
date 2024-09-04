e06c3c405a63dda7982d070bcbc1dcb119ad1b48
Anupam Raj <anupam.r2301@gmail.com>
2024-09-04 12:36:09 +0530

    add new methods

--- os.js
+++ os.js
@@ -1,3 +1,8 @@
 const os = require('os')
 const computerMemory = os.totalmem();
-console.log(Math.round(computerMemory/(1024*1024*1024)),'GB')
+console.log("Your Computer has memory of:",Math.round(computerMemory/(1024*1024*1024)),'GB')
+console.log("Your OS type:",os.type())
+console.log("OS version:",os.version());
+console.log("OS platform:",os.platform());
+console.log("CPUs:",os.cpus().length)
+console.log("CPU architecture:",os.machine())
